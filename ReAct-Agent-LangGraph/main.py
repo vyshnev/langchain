83367c448134974c9a5e24ed1159415388c03f19
Vyshnev <vyshnevnandanam@gmail.com>
2025-10-06 19:01:00 +0530

    graph

--- ReAct-Agent-LangGraph/main.py
+++ ReAct-Agent-LangGraph/main.py
@@ -1,12 +1,38 @@
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langgraph.graph import MessagesState, StateGraph, END
+
+from nodes import run_agent_reasoning, tool_node
 
 
 load_dotenv()
 
-def print_hi(name):
+AGENT_REASON = "agent_reason"
+ACT = "act"
+LAST = -1
 
-    print(f'Hi, {name}') 
 
+def should_continue(state: MessagesState) -> MessagesState:
+    if not state["messages"][LAST].tool_calls:
+        return END
+    return ACT
+
+flow = StateGraph(MessagesState)
+
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+flow.add_conditional_edges(AGENT_REASON, should_continue, {
+    END:END,
+    ACT:ACT})
+
+flow.add_edge(ACT, AGENT_REASON)
+
+app = flow.compile()
+app.get_graph().draw_png(output_file_path="flow.png")
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('Hello')
+    res = app.invoke({"messages": [HumanMessage(content="what is the temperature in Tokyo ? List it and then triple it")]})
+    print(res["messages"][LAST].content)
