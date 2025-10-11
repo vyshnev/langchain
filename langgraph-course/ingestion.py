dc3d78b83c4f716e6cc572983a47f836d2f1deb7
Vyshnev <vyshnevnandanam@gmail.com>
2025-10-11 13:12:50 +0530

    added ingestion

---
+++ langgraph-course/ingestion.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+from langchain.text_splitter import  RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+
+load_dotenv()
+
+url = [
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-06-23-agent/"
+]
+
+docs = [WebBaseLoader(url).load() for url in url]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=250, chunk_overlap=0
+)
+
+doc_split = text_splitter.split_documents(docs_list)
+
+# vectorstore = Chroma.from_documents(
+#     documents=doc_split,
+#     collection_name="rag-chroma",
+#     embedding=OpenAIEmbeddings(),
+#     persist_directory="./.chroma"
+# )
+
+retriever = Chroma(
+    collection_name="rag-chroma",
+    persist_directory="./.chroma",
+    embedding_function=OpenAIEmbeddings(),
+).as_retriever()
